6718405d12aae28b02609815b1f1755ab2465f25
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2022-11-12 22:07:59 +0700

    homepage

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import "../styles/utilities.css";
+import "../styles/homepage.css";
+import "aos/dist/aos.css";
+import AOS from 'aos';
+import { useEffect } from "react";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  useEffect(()=>{
+    AOS.init();
+    AOS.refresh();
+  }, [])
+  return <Component {...pageProps} />;
+}
