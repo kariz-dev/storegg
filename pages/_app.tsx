e5185e7a5999a69eba90251a67fe96c47e7bddb7
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2022-11-21 21:18:37 +0700

    complete checkout page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/utilities.css";
 import "../styles/homepage.css";
 import "../styles/detail.css";
 import "../styles/checkout.css";
+import "../styles/complete-checkout.css"
 import "aos/dist/aos.css";
 import AOS from 'aos';
 import { useEffect } from "react";
