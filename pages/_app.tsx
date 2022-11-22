62392117bc8a599cdee692ef62225898372bc2c5
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2022-11-22 23:33:15 +0700

    sign in page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,17 @@ import "../styles/utilities.css";
 import "../styles/homepage.css";
 import "../styles/detail.css";
 import "../styles/checkout.css";
-import "../styles/complete-checkout.css"
+import "../styles/complete-checkout.css";
+import "../styles/sign-in.css";
 import "aos/dist/aos.css";
-import AOS from 'aos';
+import AOS from "aos";
 import { useEffect } from "react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(()=>{
+  useEffect(() => {
     AOS.init();
     AOS.refresh();
-  }, [])
+  }, []);
   return <Component {...pageProps} />;
 }
