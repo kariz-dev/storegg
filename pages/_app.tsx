cb0fcc119eace6903ddf9a1976c614c5d263b0f5
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2022-11-23 21:28:54 +0700

    sign up photo page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import "../styles/checkout.css";
 import "../styles/complete-checkout.css";
 import "../styles/sign-in.css";
 import "../styles/sign-up.css";
+import "../styles/sign-up-photo.css";
 import "aos/dist/aos.css";
 import AOS from "aos";
 import { useEffect } from "react";
